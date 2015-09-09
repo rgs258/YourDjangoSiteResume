236e06c4dc096e67b68880010048a3c53c9dfcfd
Ryan Sullivan <rsull@wharton.upenn.edu>
2015-09-09 12:09:04 -0500

    Add my contact info

--- app/views.py
+++ app/views.py
@@ -8,12 +8,12 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
+    'name' : 'Ryan Sullivan',
+    'bio' : 'A person who does things...sometimes',
     'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
-    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
-    'github_username' : "fetchpush", 
-    'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
+    'twitter_username' : 'rgs258', # No @ symbol, just the handle.
+    'github_username' : "rgs258", 
+    'headshot_url' : 'https://pbs.twimg.com/profile_images/3430729352/d59992d2464d98372fd53c5dff539fbb_400x400.jpeg', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
     
 def home(request):
